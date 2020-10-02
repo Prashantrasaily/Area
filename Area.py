55ea13403489fb94c86098b9eb433fb9e916414b
Sonia22545 <72243110+Sonia22545@users.noreply.github.com>
2020-10-02 16:12:02 +0530

    updated the comments

--- Area.py
+++ Area.py
@@ -1,3 +1,3 @@
-radius = int(input("Enter the radius of circle whose area you want :"))
-area = 3.14 * radius * radius
-print(area)
+radius = int(input("Enter the radius of circle whose area you want :")) # asking for radius from the user 
+area = 3.14 * radius * radius # sice the area of circle is pi*radius*radius
+print(area) # priting the radius 
